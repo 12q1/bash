3039073bd7eb2b98207ac89f45e1299fdda26972
12q1 <thomasham89@gmail.com>
2020-02-18 21:41:50 +0100

    Adjusted powerline-shell auto install script

--- fresh_install/ubuntu_18.04/theme.sh
+++ fresh_install/ubuntu_18.04/theme.sh
@@ -17,6 +17,10 @@ sudo apt install papirus-icon-theme -y
 echo -e '\e[1;34m -materia-gtk-theme \e[0m'
 sudo apt install materia-gtk-theme -y
 
+echo -e '\e[1;34m -python-setuptools \e[0m'
+sudo apt-get install python-setuptools
+#dependency needed to install powerline-shell
+
 echo "-----------------------------------------------"
 echo -e "\e[1;36m Installing powerline-shell \e[0m"
 echo "-----------------------------------------------"
@@ -26,7 +30,7 @@ git clone https://github.com/b-ryan/powerline-shell
 cd powerline-shell
 python setup.py install
 
-echo '--------Start of Powerline script----------' >> ~/.bashrc
+echo '#--------Start of Powerline script----------' >> ~/.bashrc
 echo "function _update_ps1() { 
     PS1=\$(powerline-shell \$?) 
 } 
@@ -34,7 +38,7 @@ if [[ \$TERM != linux && ! \$PROMPT_COMMAND =~ _update_ps1 ]]; then
     PROMPT_COMMAND=\"_update_ps1; \$PROMPT_COMMAND\" 
 fi" >> ~/.bashrc
 
-echo '---------End of Powerline script-----------' >> ~/.bashrc
+echo '#---------End of Powerline script-----------' >> ~/.bashrc
 
 echo "-----------------------------------------------"
 echo -e "\e[1;36m Done \e[0m"
